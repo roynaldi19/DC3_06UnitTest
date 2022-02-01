951d11f6e2caf780f88246d92e495fdfd2cc4af7
roynaldi19 <roynaldi19@gmail.com>
2022-02-01 15:13:28 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa06_01unittest/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.roynaldi19.bfaa06_01unittest
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(private val cuboidModel: CuboidModel): ViewModel() {
+
+    fun getCircumference() = cuboidModel.getCircumference()
+
+    fun getSurfaceArea() = cuboidModel.getSurfaceArea()
+
+    fun getVolume() = cuboidModel.getVolume()
+
+    fun save(w: Double, l: Double, h: Double){
+        cuboidModel.save(w, l, h)
+    }
+}
